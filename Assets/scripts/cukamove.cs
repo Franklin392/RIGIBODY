9948c8176d7a247e35f6f7fdb1a516089eb916b4
YUHAO WU <71244025+Franklin392@users.noreply.github.com>
2020-10-21 18:02:04 -0700

    Add files via upload

---
+++ Assets/scripts/cukamove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cukamove : MonoBehaviour
+{
+    public Rigidbody cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            cube.AddForce(5, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            cube.AddForce(0, 0, 5);
+        }
+        if (Input.GetKey("space"))
+        {
+            cube.AddForce(0, 3, 0);
+        }
+    }
+}
---
+++ Assets/scripts/cykacontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cykacontroller : MonoBehaviour
+{
+    [SerializeField]
+    private float _movespeed = 5f;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        controller.Move(direction * _movespeed * Time.deltaTime);
+    }
+}
